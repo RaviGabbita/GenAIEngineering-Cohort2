ee6fe0bdfc4b63fe7c1eb5b1892ca4820a7f92b4
RaviGabbita <49125783+RaviGabbita@users.noreply.github.com>
2025-06-22 11:34:26 -0400

    Update calculator.py

--- BaseCamp3/Day_2/calculator.py
+++ BaseCamp3/Day_2/calculator.py
@@ -64,6 +64,8 @@ def calculate_result():
 
         # Make the API call
         response = requests.get(endpoint, params={"a": first_num, "b": second_num})
+        
+      #  response = requests.post(endpoint, json={"a": first_num, "b": second_num}) for 3_Clculator.py
 
         # Check if the request was successful
         if response.status_code == 200:
